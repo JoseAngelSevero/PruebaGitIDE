05b959205c4d6ab3b074002a1a209d661bcb8c14
Jose Angel <jose@severo.es>
2023-10-23 16:48:45 +0200

    V3_Implementado algoritmo de obtención del mayor de dos números.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Jose Angel!");
+        int n1, n2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserte primer número: ");
+        n1=sc.nextInt();
+        System.out.println("Inserte segundo número: ");
+        n2=sc.nextInt();
+        System.out.println("El número mayor ");
     }
 }
