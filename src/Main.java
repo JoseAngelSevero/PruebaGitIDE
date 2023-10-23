37371286db32e9e7d6e3c72eac085c2ae3d977a6
Jose Angel <jose@severo.es>
2023-10-23 17:21:42 +0200

    V5_Modificaion texto comparativa dos numeros

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int n1, n2;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Inserte primer número: ");
+        System.out.println("Inserte número primero: ");
         n1=sc.nextInt();
-        System.out.println("Inserte segundo número: ");
+        System.out.println("Inserte número segundo: ");
         n2=sc.nextInt();
         System.out.println("El número mayor ");
         System.out.println("El menor mayor ");
