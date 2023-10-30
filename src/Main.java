ed6b4f374bac9b6c9eba6ab4fbe5cf124310c692
Jose Angel <jose@severo.es>
2023-10-30 16:20:22 +0100

    "frase cambiada"

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int n1, n2;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Inserte número primero: ");
+        System.out.println("hola ");
         n1=sc.nextInt();
         System.out.println("Inserte número segundo: ");
         n2=sc.nextInt();
         System.out.println("El número mayor ");
-        System.out.println("El menor mayor ");
+        System.out.println("El menor  ");
 
     }
 }
