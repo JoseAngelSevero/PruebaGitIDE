a61d4fc139273331bc9c84fd7f292dc1a9c37a2e
Jose Angel <jose@severo.es>
2023-10-30 16:22:00 +0100

    "frase cambiada 2"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int n1, n2;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Inserte número primero: ");
+        System.out.println("Adios");
         n1=sc.nextInt();
         System.out.println("Inserte número segundo: ");
         n2=sc.nextInt();
